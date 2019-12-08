70c135f548fbc83422e6b41120af0d9d4c9b82e9
Vitor <vitorgpalves@gmail.com>
2019-12-08 15:05:47 -0300

    feature: estiliza a página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
-import { Title } from './styles'
+import { Container, Form, SubmitButton } from './styles'
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
 
-const Main = () => <Title>Main</Title>
+const Main = () => (
+  <Container>
+    <h1>
+      <FaGithubAlt />
+      Repositories
+    </h1>
+
+    <Form onSubmit={() => {}}>
+      <input type="text" placeholder="Add repository" />
+      <SubmitButton disabled={false}>
+        <FaPlus color="#FFFFFF" size={14} />
+      </SubmitButton>
+    </Form>
+  </Container>
+)
 
 export default Main
